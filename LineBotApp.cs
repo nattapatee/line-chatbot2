238bd829fe903737b67b51b9846a074fc2964b54
nattapatee <nattapat.eka@gmail.com>
2018-09-20 16:50:02 +0700

    update

--- LineBotApp.cs
+++ LineBotApp.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using centralloggerbot.CloudStorage;
 using centralloggerbot.Models;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace centralloggerbot
 {
@@ -40,7 +43,24 @@ namespace centralloggerbot
             }
             if (userMessage.ToLower() == "register")
             {
-                replyMessage.Text = "ขอบคุณที่สมัครข้อมูล เมื่อเราตรวจพบ Critical เราแจ้งเตือนหาท่านให้เร็วที่สุด ขอบคุณครับ";
+                var message = new
+                {
+                    LineId = userId
+                };
+                using (var client = new HttpClient())
+                {
+                    var data = JsonConvert.SerializeObject(message);
+                    var fullUrl = $"htps://centralloggerazure.azurewebsites.net/api/logger/addLine";
+                    var response = await client.PostAsync(fullUrl, new StringContent(data, Encoding.UTF8, "application/json"));
+                    if (response.IsSuccessStatusCode)
+                    {
+                        replyMessage.Text = "ขอบคุณที่สมัครข้อมูล เมื่อเราตรวจพบ Critical เราแจ้งเตือนหาท่านให้เร็วที่สุด ขอบคุณครับ";
+                    }
+                    else
+                    {
+                        replyMessage.Text = "พบปัญหาในการสมัครรับข้อมูล";
+                    }
+                }
             }
 
             await messagingClient.ReplyMessageAsync(replyToken, new List<ISendMessage> { replyMessage });
