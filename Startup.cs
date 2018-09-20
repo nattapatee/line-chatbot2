caa32f3cdd24766db83df155c74e830922b2f307
nattapatee <nattapat.eka@gmail.com>
2018-09-20 16:24:25 +0700

    update

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace centralloggerbot
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, DbCreateContext db)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
