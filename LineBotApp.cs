d323668e444da60332510b6f69a13a2352bff9c5
nattapatee <nattapat.eka@gmail.com>
2018-09-24 12:58:42 +0700

    update

--- LineBotApp.cs
+++ LineBotApp.cs
@@ -86,15 +86,28 @@ namespace centralloggerbot
             }
             if (userMessage.ToLower() == "confirm")
             {
-                replyMessage = new TemplateMessage("menu", new ButtonsTemplate(
-                        title: "\"menu\"",
-                        text: "Which burger you want to eat?",
-                        actions: new List<ITemplateAction>(){
-                            new MessageTemplateAction("Cheese Burger", "cheese"),
-                            new MessageTemplateAction("Plain Burger","plain"),
-                            new MessageTemplateAction("Vegi Burger","vegi"),
-                            new MessageTemplateAction("Awesome Burger","awesome"),
-                        }));
+                RichMenu richMenu = new RichMenu()
+                {
+                    Size = ImagemapSize.RichMenuLong,
+                    Selected = false,
+                    Name = "nice richmenu",
+                    ChatBarText = "touch me",
+                    Areas = new List<ActionArea>()
+                    {
+                        new ActionArea()
+                        {
+                            Bounds = new ImagemapArea(0,0 ,ImagemapSize.RichMenuLong.Width,ImagemapSize.RichMenuLong.Height),
+                            Action = new PostbackTemplateAction("ButtonA", "Menu A", "Menu A")
+                        }
+                    }
+                };
+
+                var richMenuId = await messagingClient.CreateRichMenuAsync(richMenu);
+                var image = new MemoryStream(File.ReadAllBytes(@"https://thumbs.dreamstime.com/t/web-banner-marine-navy-blue-watercolor-gradient-fill-background-watercolour-stains-abstract-painted-template-paper-texture-110217861.jpg"));
+                // Upload Image
+                await messagingClient.UploadRichMenuPngImageAsync(image, richMenuId);
+                // Link to user
+                await messagingClient.LinkRichMenuToUserAsync(userId, richMenuId);
             }
             if (userMessage.ToLower() == "message")
             {
